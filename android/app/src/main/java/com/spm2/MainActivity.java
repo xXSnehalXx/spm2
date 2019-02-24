d3cc11b4cb35d2a25fb934c318168974bbed60ff
xXSnehalXx <snehalmachan@gmail.com>
2019-02-24 18:17:10 +0530

    com1

--- android/app/src/main/java/com/spm2/MainActivity.java
+++ android/app/src/main/java/com/spm2/MainActivity.java
@@ -3,9 +3,6 @@ package com.spm2;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen; // import this
 import android.os.Bundle; // import this
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +18,4 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "spm2";
     }
-     @Override
-     protected ReactActivityDelegate createReactActivityDelegate() {
-   return new ReactActivityDelegate(this, getMainComponentName()) {
-     @Override
-     protected ReactRootView createRootView() {
-      return new RNGestureHandlerEnabledRootView(MainActivity.this);
-     }
-   };
- }
 }
